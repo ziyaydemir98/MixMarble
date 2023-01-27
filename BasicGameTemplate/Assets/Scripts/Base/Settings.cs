51d90a5c52634e828234e2a52202e305454a2b08
Muhammed İkbal KAPAR <74452587+Dukvah@users.noreply.github.com>
2023-01-27 17:18:51 +0300

    Initial commit

---
+++ BasicGameTemplate/Assets/Scripts/Base/Settings.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Settings : MonoBehaviour
+{
+    private enum Tutorial { HorizontalSwerve = 0, JoystickSwerve = 1, Drag = 2 }
+    [SerializeField] private Tutorial tutorial;
+
+    [Range(60, 120)]
+    [SerializeField] private int TFrame = 60;
+
+    private void Start()
+    {
+        Application.targetFrameRate = TFrame;
+    }
+
+    public int GetTutorialIndex()
+    {
+        return (int)tutorial;
+    }
+}
